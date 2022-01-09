d5179c746ac77d33482bdc234bcf675fd09f3bc6
dennysferrer <dennys.ferrer@gmail.com>
2022-01-09 00:07:08 -0500

    Se actualizó el codigo para mostrar los resultados en el archivo html

--- figuras.js
+++ figuras.js
@@ -1,33 +1,45 @@
 //Codigo del cuadrado
 
-const cuadrado = (ladoCuadrado) => {
-    console.group("Cuadrados");
-    console.log(`Los lados del cuadrado miden ${ladoCuadrado}`);
-    const areaCuadrado = ladoCuadrado*ladoCuadrado;
-    console.log(`El area del cuadrado es ${areaCuadrado}`);
-    const perimetroCuadrado = ladoCuadrado*4;
-    console.log(`El perímetro del cuadrado es ${perimetroCuadrado}`);
-    console.groupEnd();
+const calcularPerimetroCuadrado = () => {
+    const ladoCuadrado = document.getElementById("InputCuadrado").value;
+    const div = document.getElementById("ResultadoPerimetro");
+    const perimetroCuadrado = ladoCuadrado*4;;
+    console.log(`El perimetro del cuadrado es ${perimetroCuadrado}`);
+    div.innerHTML=`<p>${perimetroCuadrado}</p>`;
 }
 
-cuadrado(5);
+const calcularAreaCuadrado = () => {
+    const ladoCuadrado = document.getElementById("InputCuadrado").value;
+    const div = document.getElementById("ResultadoArea");
+    const areaCuadrado = ladoCuadrado*ladoCuadrado;
+    console.log(`El área del cuadrado es ${areaCuadrado}`);
+    div.innerHTML=`<p>${areaCuadrado}</p>`;
+}
 
 //Codigo del triangulo del
 
-const triangulo = (ladoTriangulo1, ladoTriangulo2, baseTriangulo) => {
-    console.group("Triangulos");
-
+const calcularPerimetroTriangulo = () => {
+    const ladoTriangulo1 = document.getElementById("Lado1").value;
+    const ladoTriangulo2 = document.getElementById("Lado2").value;
+    const baseTriangulo = document.getElementById("base").value;    
+    const div = document.getElementById("ResultadoPerimetroTriangulo")
     console.log(`Los lados del triangulo miden: Lado 1: ${ladoTriangulo1}, Lado 2: ${ladoTriangulo2}, base: ${baseTriangulo}`);
     const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+baseTriangulo;
+    div.innerHTML = `<p>${perimetroTriangulo}</p>`;
+    
+}
+
+const calcularAreaTriangulo = () => {
+    const ladoTriangulo1 = document.getElementById("Lado1").value;
+    const ladoTriangulo2 = document.getElementById("Lado2").value;
+    const baseTriangulo = document.getElementById("base").value;    
+    const div = document.getElementById("ResultadoAreaTriangulo")
     const x1 = ((ladoTriangulo1*ladoTriangulo1)+(baseTriangulo*baseTriangulo)-(ladoTriangulo2*ladoTriangulo2))/(2*baseTriangulo);
     const alturaTriangulo = Math.sqrt((ladoTriangulo1*ladoTriangulo1)-(x1*x1));
     const areaTriangulo = (baseTriangulo*alturaTriangulo)/2;
     console.log(`El área del Triangulo es: ${areaTriangulo}`);
-    console.log(`El perimetro del Triangulo es: ${perimetroTriangulo}`);
-
-    console.groupEnd();
+    div.innerHTML = `<p>${areaTriangulo}</p>`;
 }
 
-triangulo(5,5,9);
 
 
